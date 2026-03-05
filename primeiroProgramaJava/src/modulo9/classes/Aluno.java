f3c6a70f69a69494aae8423b9da50d9d99fd0689
olive <olive@Oliveira>
2026-03-05 11:24:36 -0300

    Criado modo construtores de 1parametro e 2 parametros

--- primeiroProgramaJava/src/modulo9/classes/Aluno.java
+++ primeiroProgramaJava/src/modulo9/classes/Aluno.java
@@ -2,6 +2,27 @@ package modulo9.classes;
 
 public class Aluno {
 	
-	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatricula;
 
+	public Aluno() {
+		
+	}
+	
+	public Aluno (String nomePadrao) {
+		nome = nomePadrao;
+	}
+	
+	public Aluno (String nomePadrao, int idadePadrao) {
+		nome = nomePadrao;
+		idade = idadePadrao;
+	}
 }
---
+++ primeiroProgramaJava/src/modulo9/executavel/ClasseAlunoExecutavel.java
@@ -0,0 +1,27 @@
+package modulo9.executavel;
+
+import modulo9.classes.Aluno;
+
+public class ClasseAlunoExecutavel {
+	
+	public static void main(String[] args) {
+		
+		/*Objeto ainda não existe na memória
+		Aluno aluno1;
+		
+		Agora temos um objetoo real na memoria
+		Aluno aluno2 = new Aluno();
+		*/
+		
+		Aluno aluno1 = new Aluno();//Aluno Bernardo
+		
+		Aluno aluno2 = new Aluno();//Aluno Pierre
+		
+		Aluno aluno3 = new Aluno();//Aluno Luana
+		
+		Aluno aluno4 = new Aluno("Jonas");
+		
+		Aluno aluno5 = new Aluno("Pedro", 45);
+	}
+
+}
